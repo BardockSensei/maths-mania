f1f771b48689b782c351fd7f571ebe137f58c28f
Guillaume Hostache <guillaume.hostache@outlook.fr>
2025-01-10 16:22:53 +0100

    app(v0) - architecture base

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    RouterLink,
+    MatMenuModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'maths-mania';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LessonsComponent } from './lessons/lessons.component';
+import { GamesComponent } from './games/games.component';
+import { ExercisesComponent } from './exercises/exercises.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'lessons', component: LessonsComponent },
+  { path: 'lessons/:level', component: LessonsComponent },
+  { path: 'games', component: GamesComponent },
+  { path: 'games/:game', component: GamesComponent },
+  { path: 'exercises', component: ExercisesComponent },
+  { path: 'exercises/:exercise', component: ExercisesComponent },
+];
